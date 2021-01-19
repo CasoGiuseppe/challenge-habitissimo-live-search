fdef90e5d5f7690958c5c185c4c873ed127e52b5
Giuseppe Caso <gcaso@MacBook-Pro.local>
2021-01-19 15:01:35 +0100

    merge with feature/build_mock_view and fix spaces bugs

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 // modules
-import { cosmetic } from './modules/cosmetic/index'
-Vue.use(Vuex)
+import { cosmetic } from './modules/cosmetic/index';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    cosmetic
-  }
-})
+    cosmetic,
+  },
+});
